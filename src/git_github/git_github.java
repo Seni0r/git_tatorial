2ff2af3b3a6180d6b2637b491c640b22440a9bdd
waleed <n.o.w15241@gmail.com>
2020-02-09 18:14:36 +0300

    sgaf branch

--- src/git_github/git_github.java
+++ src/git_github/git_github.java
@@ -7,7 +7,8 @@ public class git_github {
 	public static void main(String args[]) {
 		
 		System.out.println("waleed");
-		
+		System.out.println("sgaf");
+		System.out.println("sgaf brand");
 	}
 	
 	public static void name() {
